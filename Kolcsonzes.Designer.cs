002e404427168b6d7acaf1970d3f3a08ca4b0f21
AronPinter <72333153+AronPinter@users.noreply.github.com>
2021-01-06 00:52:29 +0100

    Add files via upload

---
+++ Kolcsonzes.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace KonyvtarCucc
+{
+    partial class Kolcsonzes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 81);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(317, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(336, 80);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(412, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(312, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(336, 113);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 3;
+            // 
+            // Kolcsonzes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 353);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Kolcsonzes";
+            this.Text = "Kolcsonzes";
+            this.Load += new System.EventHandler(this.Kolcsonzes_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+    }
+}
---
+++ Kolcsonzes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KonyvtarCucc
+{
+    public partial class Kolcsonzes : Form
+    {
+
+        List<Person> emberek;
+        List<Book> konyvek;
+        List<Kolcsonzott> kikolcsonzesek;
+
+        public Kolcsonzes(List<Book> konyvekref,List<Person> emberekref, List<Kolcsonzott> kikolcsonzesekref)
+        {
+            InitializeComponent();
+            button1.Text = "Könyv kölcsönzése";
+            emberek = emberekref;
+            konyvek = konyvekref;
+            kikolcsonzesek = kikolcsonzesekref;
+        }
+
+        private void Kolcsonzes_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            
+            for (int i = 0; i < emberek.Count; ++i)
+                comboBox1.Items.Add(emberek[i].vezetekNev + " " + emberek[i].keresztNev + " " + emberek[i].levelezesiCim);
+
+            comboBox2.Items.Clear();
+            for (int i = 0; i < konyvek.Count; ++i)
+                comboBox2.Items.Add(konyvek[i].Szerzo + " " + konyvek[i].Name + " " + konyvek[i].KiadasiEv);
+
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Kolcsonzott newdata = new Kolcsonzott();
+            newdata.ember = emberek[comboBox1.SelectedIndex];
+            newdata.konyv = konyvek[comboBox2.SelectedIndex];
+            newdata.kikolcsonzesIdeje = DateTime.Today;
+            newdata.lejarat = monthCalendar1.SelectionStart;
+            kikolcsonzesek.Add(newdata);
+        }
+    }
+}
